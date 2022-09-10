c23e83bbbd53e8ef81c85f61383acd9d42a7a4b0
Sam M <smeador11@gmail.com>
2022-09-10 11:32:43 -0400

    Initial MobileDev repo commit

---
+++ UdemyCourses/MobileDevelopment/iOS/I_Am_Rich/I_Am_Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_Am_Rich
+//
+//  Created by Sam Meador on 9/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
